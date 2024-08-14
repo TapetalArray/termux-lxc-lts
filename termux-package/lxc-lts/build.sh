2b5467f5d940fe01c390f419d1283c8082fc884b
TapetalArray <61140679+TapetalArray@users.noreply.github.com>
2024-08-14 16:11:05 +0800

    Update build.sh

--- termux-package/lxc-lts/build.sh
+++ termux-package/lxc-lts/build.sh
@@ -6,11 +6,11 @@ TERMUX_PKG_VERSION=6.0.1
 TERMUX_PKG_SRCURL=https://linuxcontainers.org/downloads/lxc/lxc-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ccb38fbcdb86a82ee8192ccc85bba47edaf824439e9a7f12ab178d51ff1f77e0
 TERMUX_PKG_RECOMMENDS="gnupg, wget"
+TERMUX_PKG_DEPENDS="dbus"
 TERMUX_PKG_NO_STATICSPLIT=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dinit-script=sysvinit
 -Druntime-path=$TERMUX_PREFIX/tmp
 --localstatedir=$TERMUX_PREFIX/var
--Ddbus=false
 -Dman=false
 "
